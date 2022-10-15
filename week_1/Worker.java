eb1977cb59e0f5aa3c3a47c4f380f301bbe49e0c
Alper-tt <alpertopraktepe46@gmail.com>
2022-10-16 00:51:25 +0300

    week1

---
+++ week_1/Worker.java
@@ -0,0 +1,16 @@
+package week_1;
+
+public class Worker {
+
+    String name;
+    int socialSecurityNumber;
+    int workingHours;
+    float wage;
+    
+    public void displayInfo(){
+        System.out.println("SSN : "+socialSecurityNumber+"\nname : "+name);
+    }
+    public void displaySalary(){
+        System.out.println("Salary (daily) : "+workingHours*wage);
+    }
+}
